f7c213ff7330cb1cabbd6f821e99ea9c51b08779
ACSouzaJr <junioracs2000@gmail.com>
2020-10-17 14:16:42 -0300

    feat: add onboarding

--- App.tsx
+++ App.tsx
@@ -1,22 +1,78 @@
-import React from 'react';
-import { AppLoading } from 'expo';
-import { useFonts, Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import React, { useEffect, useState } from "react";
+import { AppLoading } from "expo";
+import {
+  useFonts,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
+import Onboarding from "react-native-onboarding-swiper";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import Routes from './src/routes';
+import Routes from "./src/routes";
+import image from "./src/assets/map-marker.png";
+import { Image } from "react-native";
 
 export default function App() {
-
+  const [hasOnboarding, setHasOnboarding] = useState(false);
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
-    Nunito_700Bold, 
-    Nunito_800ExtraBold
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
   });
+
+  useEffect(() => {
+    (async () => {
+      const hasOnboarding = await AsyncStorage.getItem("@hasOnboarding");
+      if (hasOnboarding) {
+        setHasOnboarding(JSON.parse(hasOnboarding));
+      }
+    })();
+  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
+  async function compleOnboarding() {
+    try {
+      await AsyncStorage.setItem("@hasOnboarding", JSON.stringify(true));
+      setHasOnboarding(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Routes />
+    <>
+      {hasOnboarding ? (
+        <Routes />
+      ) : (
+        <Onboarding
+          onDone={compleOnboarding}
+          onSkip={compleOnboarding}
+          pages={[
+            {
+              backgroundColor: "#fff",
+              image: <Image source={image} />,
+              title: "Onboarding",
+              subtitle: "Done with React Native Onboarding Swiper",
+            },
+            {
+              backgroundColor: "#fe6e58",
+              image: <Image source={image} />,
+              title: "The Title",
+              subtitle: "This is the subtitle that sumplements the title.",
+            },
+            {
+              backgroundColor: "#999",
+              image: <Image source={image} />,
+              title: "Triangle",
+              subtitle: "Beautiful, isn't it?",
+            },
+          ]}
+        />
+      )}
+    </>
   );
 }
